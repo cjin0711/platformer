2f5954fcc8fabbb31d83c1998fa95d3479e707bf
Chris Jin <cj0711888@gmail.com>
2024-12-01 02:42:44 -0500

    added header block

--- main.cpp
+++ main.cpp
@@ -1,3 +1,13 @@
+/**
+* Author: Chris Jin
+* Assignment: Platformer
+* Date due: 2024-11-26, 11:59pm
+* I pledge that I have completed this assignment without
+* collaborating with anyone else, in conformance with the
+* NYU School of Engineering Policies and Procedures on
+* Academic Misconduct.
+**/
+
 
 #define LOG(argument) std::cout << argument << '\n'
 #define FIXED_TIMESTEP 0.0166666f
